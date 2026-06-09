c76deb8f8f05ce35373ff2bd07aaf88fdb04304c
jparedes <judith.paredes@turismo.gob.ec>
2026-06-09 13:06:52 -0500

    No puede ser obligatario provincia, canton y parroquia en los guias de canje

--- src/modules/core/roles/external/dto/process-guide/guide-origin.dto.ts
+++ src/modules/core/roles/external/dto/process-guide/guide-origin.dto.ts
@@ -2,10 +2,12 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { isNotEmptyValidationOptions, isStringValidationOptions } from '@utils/dto-validation';
 
 export class GuideOriginDto {
+  @IsOptional()
   @IsString(isStringValidationOptions())
   @IsNotEmpty(isNotEmptyValidationOptions())
   readonly province: string;
 
+  @IsOptional()
   @IsString(isStringValidationOptions())
   @IsNotEmpty(isNotEmptyValidationOptions())
   readonly canton: string;
