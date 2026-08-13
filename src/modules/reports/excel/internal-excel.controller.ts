60bd02fb13f6017758cfb5012b363088c1f5496a
jparedes <judith.paredes@turismo.gob.ec>
2026-08-13 14:30:02 -0500

    Reporte de bandejas

--- src/modules/reports/excel/internal-excel.controller.ts
+++ src/modules/reports/excel/internal-excel.controller.ts
@@ -1,35 +1,25 @@
 import { Controller, Get, Header, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiTags } from '@nestjs/swagger';
-import { PublicRoute, User } from '@auth/decorators';
+import { User } from '@auth/decorators';
 import { InternalExcelService } from '@modules/reports/excel/internal-excel.service';
 import { UserEntity } from '@auth/entities';
-import { FindProcessesDto } from '@modules/core/roles/guide-technician/dto/guide-technician/find-processes.dto';
 
 @ApiTags('Internal EXCEL Reports')
 @Controller('reports/excel/internals')
 export class InternalExcelController {
   constructor(private readonly service: InternalExcelService) {}
 
-  @PublicRoute()
   @Header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+  @Header('Content-Disposition', 'attachment; filename="bandeja_tecnico.xlsx"')
   @Get('technician-guide/processes')
   async generateProcessesByTechnicianGuide(
     @Res() response: Response,
     @User() user: UserEntity,
-    @Query() params: FindProcessesDto,
+    @Query('rolCode') rolCode: string,
+    @Query('isCurrent') isCurrent: boolean,
   ) {
-    const excelBuffer = await this.service.generateProcesses(
-      user.id,
-      params.rolCode,
-      params.isCurrent,
-    );
-
-    response.setHeader(
-      'Content-Disposition',
-      'attachment; filename="registration-certificate.xlsx"',
-    );
-
+    const excelBuffer = await this.service.generateProcesses(user.id, rolCode, isCurrent);
     response.send(excelBuffer);
   }
 }
--- src/modules/reports/excel/templates/processes.report.ts
+++ src/modules/reports/excel/templates/processes.report.ts
@@ -1,15 +1,19 @@
-import { differenceInCalendarDays } from 'date-fns';
+import { differenceInCalendarDays, differenceInDays } from 'date-fns';
 
 export const processesReport = (data: any) => {
   return data.map((item) => ({
-    'Número de Registro': item.process.cadastre,
+    'Número de Registro': item.process?.cadastre
+      ? item.process?.cadastre.registerNumber
+      : 'Sin registro',
     'Código del Establecimiento': item.process.establishment.number,
     'Razón Social': item.process.establishment.ruc.legalName,
-    'Ubicación': item.process.establishment.province,
-    'Clasificación': item.process.credentials.map((item1) => item1.classification.name).join(', '),
-    'Trámite': item.process.type.name,
-    'Estado del Registro': item.process.cadastre.state.name,
+    Ubicación: `${item.process.establishment.province.name} ${item.process.establishment.canton.name} ${item.process.establishment.parish.name}` ,
+    Clasificación: item.process.credentials.map((item1) => item1.classification.name).join(', '),
+    Trámite: item.process.type.name,
+    'Estado del Registro': item.process?.cadastre?.state
+      ? item.process.cadastre.state.name
+      : 'Sin registro',
     'Fecha solicitud': item.process.createdAt,
-    'Tiempo transcurrido': differenceInCalendarDays(new Date(), item.process.createdAt),
+    'Tiempo transcurrido': `${differenceInDays(new Date(), item.process.createdAt)} días`,
   }));
 };
